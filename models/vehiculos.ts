d656a1f443f9747c61e875ce65ac5e1e26d00dbd
Luis Miguel Alvarez <lmiguelalvc@gmail.com>
2025-12-01 17:24:06 -0500

    Modificaciones a El modelo

--- models/vehiculos.ts
+++ models/vehiculos.ts
@@ -1,3 +1,18 @@
-export type vehiculo = {
-
+export type Vehiculo = {
+    Marca: string;
+    Modelo: string;
+    Placa: string;
+    NombreDelDueño: string;
+    Año: number;
+    TipoDeGasolina: string;
 }
+
+export const emptyVehiculo: Vehiculo = {
+    Marca: '',
+    Modelo: '',
+    Placa: '',
+    NombreDelDueño: '',
+    Año: 0,
+    TipoDeGasolina: ''
+}
+
